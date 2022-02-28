3c3cc658e5301c72ede6713b995b9021588d2d2a
Aidan Joffee <19011337@vcconnect.co.za>
2022-02-28 12:38:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wordle/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wordle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //----------------------------------------------------------------------------------
+    //onCreate method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//--------------------------------------end of file--------------------------------------------
